b6f56009618bf672a9185fe7071a8cbbd784e661
Gustavo Tempel <45667766+gustavotempel@users.noreply.github.com>
2019-04-23 16:32:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return 'Hola Mundo!'
 
-@app.route('/getjson/', methods = ['GET', 'POST'])
+@app.route('/getjson', methods = ['GET', 'POST'])
 def getjson():
     return jsonify(
         username = 'Gustavo',
@@ -15,8 +15,8 @@ def getjson():
         email = 'gtempel@hotmail.com'
     )
 
-@app.route('/csv2json/', methods = ['GET','POST'])
-def convertCsvJson():
+@app.route('/csv2jsonFile', methods = ['GET','POST'])
+def convertCsvJsonFile():
     csvFile = "libro1.csv"
     jsonFile = "libro1.json"
 
@@ -27,6 +27,14 @@ def convertCsvJson():
     out = json.dumps([row for row in reader], indent = 4)
     jsonfile.write(out)
     return "Convertido!"
+
+@app.route('/csv2json', methods = ['GET','POST'])
+def convertCsvJson():
+    if request.method == 'POST':
+        data = request.data
+        #reader = csv.DictReader(data) json.dumps(data)
+    return data
+    
 
 if __name__ == '__main__':
     app.run(debug = True, port = 8000)
